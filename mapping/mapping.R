cd3a104225624e1a5a1d4a8193cf9e71601288b7
Andrea Odell <anodell@ucdavis.com>
2022-09-22 15:46:43 -0700

    Create a map of the coastline

---
+++ mapping/mapping.R
@@ -0,0 +1,65 @@
+library("rgdal") # for `ogrInfo()` and `readOGR()`
+library("tools") # for `file_path_sans_ext()`
+library("dplyr") # for `inner_join()`, `filter()`, `summarise()`, and the pipe operator (%>%)
+library("ggplot2") # for `fortify()` and for plotting
+library("sp") # for `point.in.polygon()` and `spDists()`
+library("tidyr") # for `gather()`
+library("readr") # for `write_tsv()`
+library("mapproj") # for ggplot:coord_map()
+
+
+fortify.shape <- function(x){
+  x@data$id <- rownames(x@data)
+  x.f <- fortify(x, region = "id")
+  x.join <- inner_join(x.f, x@data, by = "id")
+}
+
+subset.shape <- function(x, domain){
+  x.subset <- filter(x, long > domain[1] & 
+                       long < domain[2] & 
+                       lat > domain[3] & 
+                       lat < domain[4])
+  x.subset
+}
+
+
+path.ne.coast <- ("mapping/ne_10m_coastline/")
+fnam.ne.coast <- "ne_10m_coastline.shp"
+dat.coast <- readOGR(dsn = path.ne.coast, 
+                     layer = file_path_sans_ext(fnam.ne.coast))
+# A Large SpatialLinesDataFrame object with 4132 features and 2 fields (12.8 Mb)
+
+# Fortify the shapefile data using `fortify.shape()`:
+dat.coast <- fortify.shape(dat.coast) # a 410951x8 dataframe
+
+# Specify the desired domain (the West Coast of the USA):
+domain <- c(-128, -115, 29, 60)
+
+# Extract the coastline data for the desired domain using `subset.shape()`:
+dat.coast.wc <- subset.shape(dat.coast, domain) # a 4871x8 dataframe
+
+
+# Specify the spatial extent for our map (i.e., our study area; notice that its
+# dimensions are different from the domain for which we extracted the
+# coastline):
+xlims <- c(-135, -116)
+ylims <- c(32.5, 56)
+
+# Generate a base map with the coastline:
+p0 <- ggplot() + 
+  geom_path(data = dat.coast.wc, aes(x = long, y = lat, group = group), 
+            color = "black", size = 0.25) + 
+  coord_map(projection = "mercator") + 
+  scale_x_continuous(limits = xlims, expand = c(0, 0)) + 
+  scale_y_continuous(limits = ylims, expand = c(0, 0)) + 
+  labs(list(title = "", x = "Longitude", y = "Latitude")) +
+  geom_point(data = hake_weight_age_df, aes(x = (hb_longitude*-1), y = hb_latitude, color = weight))
+p0
+
+
+
+
+
+
+
+
