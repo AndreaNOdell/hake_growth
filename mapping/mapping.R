bfdef5874b8e3a166c86a0979d1f02be512ced25
AndreaNOdell <anodell@ucdavis.edu>
2023-09-20 15:45:37 -0700

    update map with shapefiles that include canada. Explore model outputs via plots

--- mapping/mapping.R
+++ mapping/mapping.R
@@ -23,8 +23,8 @@ subset.shape <- function(x, domain){
 }
 
 
-path.ne.coast <- ("mapping/ne_10m_coastline/")
-fnam.ne.coast <- "ne_10m_coastline.shp"
+path.ne.coast <- ("mapping/stanford-ns372xw1938-shapefile/")
+fnam.ne.coast <- "ns372xw1938.shp"
 dat.coast <- readOGR(dsn = path.ne.coast, 
                      layer = file_path_sans_ext(fnam.ne.coast))
 # A Large SpatialLinesDataFrame object with 4132 features and 2 fields (12.8 Mb)
@@ -33,7 +33,7 @@ dat.coast <- readOGR(dsn = path.ne.coast,
 dat.coast <- fortify.shape(dat.coast) # a 410951x8 dataframe
 
 # Specify the desired domain (the West Coast of the USA):
-domain <- c(-128, -115, 29, 60)
+domain <- c(-135, -115, 29, 60)
 
 # Extract the coastline data for the desired domain using `subset.shape()`:
 dat.coast.wc <- subset.shape(dat.coast, domain) # a 4871x8 dataframe
@@ -52,26 +52,84 @@ ylims <- c(32.5, 56)
 # Generate a base map with the coastline:
 year_sampled = as.numeric(sort(unique(hake_weight_age_df$catch_year)))
 p0 <- ggplot() + 
-  geom_point(data = df[df$catch_year %in% year_sampled,], aes(x = (hb_longitude*-1), y = hb_latitude, color = omega_s), 
-             size = 0.8, shape = 15) +
+  geom_point(data = pred_out[pred_out$catch_year %in% year_sampled,], aes(x = hb_longitude, y = hb_latitude, color = omega_s), 
+             size = 0.8, shape = 15) + 
   geom_path(data = dat.coast.wc, aes(x = long, y = lat, group = group), 
             color = "black", size = 0.25) + 
   coord_map(projection = "mercator") + 
   scale_x_continuous(limits = xlims, expand = c(0, 0)) + 
   scale_y_continuous(limits = ylims, expand = c(0, 0)) + 
-  labs(x = "Longitude", y = "Latitude") +
-  scale_colour_gradient2(low = "red", mid = "white",  high = "blue") +
-  #facet_wrap(~catch_year) +
-  theme_classic()
+  labs(x = "Longitude", y = "Latitude", color = "Spatial RE") +
+  scale_color_gradient2(low = "red", mid = "white",  high = "blue") +
+  #scale_color_viridis_c() +
+  #facet_wrap(~catch_year, ncol = 5) +
+  theme_classic() +
+  theme(axis.text=element_text(size=12),
+        axis.title = element_text(size=12)) 
 
-
-jpeg("plots/nested_models/cohort_month_sex_model/spatialtrends.jpeg", units="in", width=8, height=8, res=300)
+jpeg("plots/output_exploration/spatiotemporalRE_map.jpeg", units="in", width=8, height=8, res=300)
 p0
 dev.off()
 
+jpeg("plots/output_exploration/month_effect.jpeg", units="in", width=6, height=4, res=300)
+pred_out %>% 
+  group_by(catch_month) %>% 
+  summarise(mean_est = mean(plogis(est)), sd_est = sd(plogis(est))) %>% 
+  ggplot(aes(x = catch_month, y = mean_est)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_est-sd_est, ymax = mean_est+sd_est)) +
+  theme_classic() +
+  labs(x = "month", y = "mean predicted weight (+/- SE)")
+dev.off()
 
+jpeg("plots/output_exploration/year_effect.jpeg", units="in", width=6, height=4, res=300)
+pred_out %>% 
+  group_by(catch_year) %>% 
+  filter(catch_year %in% year_sampled) %>% 
+  summarise(mean_est = mean(plogis(est)), sd_est = sd(plogis(est))) %>% 
+  ggplot(aes(x = catch_year, y = mean_est)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_est-sd_est, ymax = mean_est+sd_est)) +
+  theme_classic() +
+  labs(x = "year", y = "mean predicted weight (+/- SE)")
+dev.off()
 
+jpeg("plots/output_exploration/age_effect.jpeg", units="in", width=6, height=4, res=300)
+pred_out %>% 
+  group_by(new_age) %>% 
+  summarise(mean_est = mean(plogis(est)), sd_est = sd(plogis(est))) %>% 
+  ggplot(aes(x = new_age, y = mean_est)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_est-sd_est, ymax = mean_est+sd_est)) +
+  theme_classic() +
+  labs(x = "age", y = "mean predicted weight (+/- SE)")
+dev.off()
 
+jpeg("plots/output_exploration/cohort_effect.jpeg", units="in", width=6, height=4, res=300)
+pred_out %>% 
+  group_by(cohort) %>% 
+  summarise(mean_est = mean(plogis(est)), sd_est = sd(plogis(est))) %>% 
+  ggplot(aes(x = cohort, y = mean_est)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_est-sd_est, ymax = mean_est+sd_est)) +
+  theme_classic() +
+  labs(x = "cohort", y = "mean predicted weight (+/- SE)")
+dev.off()
 
+# Q-Q plot -------
+set.seed(1)
+rq_res <- residuals(m_age_month_cohort) # randomized quantile residuals
+jpeg("plots/output_exploration/Q-Qplot.jpeg", units="in", width=6, height=4, res=300)
+qqnorm(rq_res, ylim = c(-10,10));qqline(rq_res)
+dev.off()
+
+# Weight histogram --------------
+jpeg("plots/output_exploration/predicted_weight_histogram.jpeg", units="in", width=6, height=4, res=300)
+hist(plogis(pred_out$est), xlab = "Weight", main = "Predicted Weights", xlim = c(0,1.5), breaks = 20)
+dev.off()
+
+jpeg("plots/output_exploration/observed_weight_histogram.jpeg", units="in", width=6, height=4, res=300)
+hist(hake_weight_age_df$weight, xlim = c(0,1.5), main = "Observed Weights", breaks = 30)
+dev.off()
 
 
