e954990bfcd4eefb5636056faef8de3d33a433a2
AndreaNOdell <anodell@ucdavis.edu>
2023-10-13 14:56:43 -0700

    create prediction grid for fishery data

--- scripts/pred_grid.R
+++ scripts/pred_grid.R
@@ -9,24 +9,25 @@ library(ggpubr)
 library(ggbreak)
 source("malick_code/functions.R")
 
-# Load data
-load("results/sdmTMB/m_age_month_cohort.RData")
-load("results/hake_weight_age_df.RData")
-model = m_age_month_cohort
+# Survey -----------
+# Load data 
+load("results/sdmTMB/m_age_month_cohort_sex_insmooth.RData")
+load("results/hake_weight_age_df_updated.RData")
+model = m_age_month_cohort_sex_insmooth
 dat = model$data
 
 # get predictions for current observations
 pred <- predict(model)
 
 # Gather unique combinations of covariates
-dat$year_age_cohort_month_sex <- as.factor(paste(dat$catch_year, dat$new_age, dat$cohort, dat$catch_month)) #, dat$sex_complete, dat$X, dat$Y
+dat$year_age_cohort_month_sex <- as.factor(paste(dat$catch_year, dat$new_age, dat$cohort, dat$catch_month, dat$sex_description)) #, dat$sex_complete, dat$X, dat$Y
 
 # Create spatial grid
 grid_pred_full <- pred_grid(x = dat$X*1000, y = dat$Y*1000,
                             res =5e3, plot = FALSE)
 names(grid_pred_full) <- c("easting_m", "northing_m")
 grid_pred_full$cell_id <- 1:nrow(grid_pred_full)
-plot(grid_pred_full$easting_m, grid_pred_full$northing_m)
+#plot(grid_pred_full$easting_m, grid_pred_full$northing_m)
 
 
 excl <- exclude_grid(grid_pred_full$easting_m,
@@ -41,7 +42,7 @@ grid_pred <- grid_pred_full[!excl, ]
 row.names(grid_pred) <- NULL
 
 ## Plot grid
-plot(grid_pred$easting_m, grid_pred$northing_m)
+#plot(grid_pred$easting_m, grid_pred$northing_m)
 
 grid_pred = grid_pred %>% 
   mutate(coords = paste(easting_m, northing_m)) %>% 
@@ -53,6 +54,7 @@ df$catch_year <- as.numeric(unlist(lapply(strsplit(as.character(df$year_age_coho
 df$new_age <- as.numeric(unlist(lapply(strsplit(as.character(df$year_age_cohort_month_sex), " "), getElement, 2)))
 df$cohort <- unlist(lapply(strsplit(as.character(df$year_age_cohort_month_sex), " "), getElement, 3))
 df$catch_month <- unlist(lapply(strsplit(as.character(df$year_age_cohort_month_sex), " "), getElement, 4))
+df$sex_description <- unlist(lapply(strsplit(as.character(df$year_age_cohort_month_sex), " "), getElement, 5))
 #df$X <- unlist(lapply(strsplit(as.character(df$year_age_cohort_month_sex), " "), getElement, 5))
 #df$Y <- unlist(lapply(strsplit(as.character(df$year_age_cohort_month_sex), " "), getElement, 6))
 df$cell = 1:nrow(df)
@@ -69,12 +71,14 @@ pred_grid = full_join(spat_df, df)
 pred_grid$new_age <- as.integer(pred_grid$new_age)
 pred_grid$cohort <- as.integer(pred_grid$cohort)
 pred_grid$catch_month <- as.numeric(as.character(pred_grid$catch_month))
-#pred_grid$sex_complete <- as.factor(pred_grid$sex_complete)
+pred_grid$sex_description <- as.factor(pred_grid$sex_description)
 pred_grid$catch_year <- as.numeric(pred_grid$catch_year)
 #pred_grid$X <- as.numeric(pred_grid$X)
 #pred_grid$Y <- as.numeric(pred_grid$Y)
 
 pred_out <- predict(model, newdata = pred_grid, return_tmb_object = FALSE)#, se_fit = TRUE)
+years_sampled = as.numeric(unique(hake_weight_age_df_updated$catch_year))
+pred_out = pred_out[pred_out$catch_year %in% years_sampled,]
 coords = cbind(pred_out$X, pred_out$Y)
 sputm <- SpatialPoints(coords, proj4string=CRS("+proj=utm +zone=10 +datum=WGS84 +units=km"))
 spgeo <- as.data.frame(spTransform(sputm, CRS("+proj=longlat +datum=WGS84")))
@@ -82,72 +86,13 @@ coords = cbind(as.data.frame(sputm), spgeo)
 names(coords) = c("X_test", "Y_test", "hb_longitude", "hb_latitude")
 pred_out = cbind(pred_out, coords[,3:4]) 
 
-# Index  ----------------
-# Whole spatial domain
-years_sampled = sort(unique(hake_weight_age_df$catch_year))
-pred_sims <- predict(model, newdata = pred_grid, return_tmb_object = TRUE)
-index_sims = get_index(pred_sims)
-index_sims = index_sims %>% 
-  filter(catch_year %in% years_sampled) %>% 
-  mutate(region = "All")
+# Fishery -------------
+load("results/sdmTMB/fm_age_sex_cohort_month_yearre.RData")
 
-n_peryear = pred_grid %>% 
-  filter(catch_year %in% years_sampled) %>% 
-  group_by(catch_year) %>% 
-  summarise(n = n()) %>% 
-  mutate(region = "All")
+model = fm_age_sex_cohort_month_yearre
+dat = model$data
 
-
-#  Canada
-pred_grid_north =  pred_grid %>% 
-  filter(Y > 5437.8)  # 547 is the UTM coordinate associated with 49 latitude
-pred_sims_north = predict(model, newdata = pred_grid_north, return_tmb_object = TRUE)
-index_sims_north = get_index(pred_sims_north)
-index_sims_north = index_sims_north %>% 
-  filter(catch_year %in% years_sampled) %>% 
-  mutate(region = "Canada")
-
-n_peryear_north = pred_grid_north %>% 
-  filter(catch_year %in% years_sampled) %>% 
-  group_by(catch_year) %>% 
-  summarise(n = n()) %>% 
-  mutate(region = "Canada")
-
-# USA
-pred_grid_south = pred_grid %>% 
-  filter(Y < 5437.8) 
-pred_sims_south = predict(model, newdata = pred_grid_south, return_tmb_object = TRUE)
-index_sims_south = get_index(pred_sims_south) 
-index_sims_south = index_sims_south %>% 
-  filter(catch_year %in% years_sampled) %>% 
-  mutate(region = "USA")
-
-n_peryear_south = pred_grid_south %>% 
-  filter(catch_year %in% years_sampled) %>% 
-  group_by(catch_year) %>% 
-  summarise(n = n()) %>% 
-  mutate(region = "USA")
-
-index_sims_full = rbind(index_sims, index_sims_north, index_sims_south)
-n_peryear_full = rbind(n_peryear, n_peryear_north, n_peryear_south)
-index_final = left_join(index_sims_full, n_peryear_full) %>% 
-  mutate(est_weight = est/n, lwr_weight = lwr/n, upr_weight = upr/n)
-
-#save(index_final, file = "results/sdmTMB/index_final-m_age_month_cohort.RData")
-
-
-# plot the weight index by region
-jpeg(filename = "plots/output_exploration/weight_index.jpeg", units="in", width=6, height=4, res = 300)
-ggplot(index_final, aes(x = catch_year, y = est_weight)) +
-  geom_ribbon(aes(ymin = lwr_weight, ymax = upr_weight, fill = region), alpha = 0.4) +
-  geom_line(aes(color = region)) +
-  theme_classic()
-dev.off()
-
-
-
-
-
-
+pred_out_fishery = predict(fm_age_sex_cohort_month_yearre)
+save(pred_out_fishery, file = "results/sdmTMB/pred_out_fishery.RData")
 
 
